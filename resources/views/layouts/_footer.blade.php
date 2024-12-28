f0f428d284b0fcd358a2aef00860e6500fc16b8e
fahim <fahimahamedweb@gmail.com>
2024-12-29 00:16:33 +0600

    home added

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,66 @@
+<footer class="footer footer-dark">
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget widget-about">
+              
+
+                        <div class="social-icons">
+                           
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget">
+                        <h4 class="widget-title">Useful Links</h4>
+
+                        <ul class="widget-list">
+                            <li><a href="{{ url('about') }}">About us</a></li>
+                            <!-- <li><a href="{{ url('how-to') }}">How to shop on us</a></li> -->
+                            <li><a href="{{ url('faq') }}">FAQ</a></li>
+                            <li><a href="{{ url('contact') }}">Contact us</a></li>
+                            <li><a href="#signin-modal" data-toggle="modal">Log in</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget">
+                        <h4 class="widget-title">Customer Service</h4>
+
+                        <ul class="widget-list">
+                            <li><a href="{{ url('payment-method') }}">Payment Methods</a></li>
+                            <li><a href="{{ url('money-back-guarantee') }}">Money-back guarantee!</a></li>
+                            <li><a href="{{ url('returns') }}">Returns</a></li>
+                            <li><a href="{{ url('shipping') }}">Shipping</a></li>
+                            <li><a href="{{ url('terms-conditions') }}">Terms and conditions</a></li>
+                            <li><a href="{{ url('privacy-policy') }}">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget">
+                        <h4 class="widget-title">My Account</h4>
+
+                        <ul class="widget-list">
+                            <li><a href="#signin-modal" data-toggle="modal">Sign In</a></li>
+                            <li><a href="{{ url('cart') }}">View Cart</a></li>
+                            <li><a href="{{ url('wishlist') }}">My Wishlist</a></li>
+                            <li><a href="#">Track My Order</a></li>
+                            <li><a href="#">Help</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+           
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+//Route::get('/', function () {
+    ///return view('welcome');
+//});
+// ----- Admin -----
+Route::get('/', [HomeController::class, 'home']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
