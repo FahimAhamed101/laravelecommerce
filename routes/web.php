15c92739810ec202ffd8e8197d7aa436d12dbc2e
fahim <fahimahamedweb@gmail.com>
2024-12-29 00:21:21 +0600

    authentication added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 //Route::get('/', function () {
@@ -9,3 +9,10 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'home']);
 
 
+Route::get('admin', [AuthController::class, 'login_admin']);
+Route::post('admin', [AuthController::class, 'auth_login_admin']);
+Route::get('admin/logout', [AuthController::class, 'logout_admin']);
+
+// ----- User -----
+Route::post('auth_register', [AuthController::class, 'auth_register']);
+Route::post('auth_login', [AuthController::class, 'auth_login']);
